685197ab847872bf2bad06f31542cfb6cbf28001
Robinee <yb6925@naver.com>
2023-02-20 17:31:40 +0900

    Write js for password match

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
+const pwdInput = document.getElementById("password");
+const confirmInput = document.getElementById("confirm_password");
+const errorMsg = document.querySelector(".error-msg");
 
-console.log("hello world")
+confirmInput.addEventListener("keyup", (e)=>{
+    const pwd = pwdInput.value;
+    const confirmpwd = e.target.value;
+
+    if(pwd === confirmpwd){
+        pwdInput.classList.remove("error");
+        confirmInput.classList.remove("error");
+        errorMsg.textContent = ""
+    }else{
+        if(!pwdInput.classList.contains("error")){
+            pwdInput.classList.add("error");
+            confirmInput.classList.add("error");
+            errorMsg.textContent = "Password do not match"
+        }
+    }
+})
